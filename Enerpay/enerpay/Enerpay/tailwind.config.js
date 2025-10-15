d683d132b887234c4aea9175afb6fbb20ef1fc4f
EnerPay Developer <developer@enerpay.com>
2025-10-14 21:45:05 -0600

    initial enerpay starknet project

---
+++ Enerpay/enerpay/Enerpay/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+      extend: {
+        colors: {
+          // Variables de colores personalizados
+        }
+      }
+    }
+  }
---
+++ Enerpay/enerpay/vite.config.ts
@@ -0,0 +1,7 @@
+export default defineConfig({
+    plugins: [react()],
+    server: {
+      port: 5173,
+      host: true // Para Codespaces
+    }
+  })
